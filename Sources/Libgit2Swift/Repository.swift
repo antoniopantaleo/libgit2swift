a10adf91c0172d31fd34e6a04c9fc01bc725c41c
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-06-17 11:20:39 +0200

    revert: remove Libgit2Swift enum
    This reverts 180fe32aec81c66b2feceda1f6b2bfcdee8bb394

--- Sources/Libgit2Swift/Repository.swift
+++ Sources/Libgit2Swift/Repository.swift
@@ -15,7 +15,17 @@ public actor Repository {
     private let STATUS_CODE_OK = 0
     private var repository: OpaquePointer?
     
+    deinit {
+        git_repository_free(repository)
+        git_libgit2_shutdown()
+    }
+    
+    private init() {
+        git_libgit2_init()
+    }
+    
     public init(path: URL) async throws {
+        self.init()
         try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             Task {
                 let exitCode = git_repository_open(&repository, path.path())
@@ -24,17 +34,21 @@ public actor Repository {
                     return continuation.resume(throwing: GitError.clone(message: String(cString: error!)))
                 }
                 if let repoDir = git_repository_path(repository) {
-                    logger.info("Repository cloned at \(String(cString: repoDir))")
+                    logger.info("Repository opened at \(String(cString: repoDir))")
                 }
                 continuation.resume()
             }
         }
     }
     
-    public init(clone: URL, path: URL) async throws {
+    public init(clone repo: URL, path: URL) async throws {
+        self.init()
         try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             Task {
-                let exitCode = git_clone(&repository, clone.absoluteString, path.path(), nil)
+                let now = Date.now
+                logger.info("Prepare to clone repo \(repo)")
+                let exitCode = git_clone(&repository, repo.absoluteString, path.path(), nil)
+                logger.info("Finished cloning \(repo) in \(now.distance(to: Date.now))")
                 if exitCode != STATUS_CODE_OK {
                     let error = git_error_last().pointee.message
                     return continuation.resume(throwing: GitError.clone(message: String(cString: error!)))
