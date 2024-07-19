b1ee8fbe09f38a929bd6426e104404a97a36ebd9
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-07-19 22:00:05 +0200

    refactor(internal): improve path(relativeTo) method implementation

--- Sources/Libgit2Swift/Internal/URL+Extensions.swift
+++ Sources/Libgit2Swift/Internal/URL+Extensions.swift
@@ -9,11 +9,11 @@ import Foundation
 
 extension URL {
     func path(relativeTo url: URL) -> URL? {
-        guard let baseRegex = try? Regex(url.path(percentEncoded: false) + "/") else { return nil }
-        let path = self.path(percentEncoded: false)
+        guard let baseRegex = try? Regex(url.path() + "/") else { return nil }
+        let path = self.path()
         guard let match = path.firstMatch(of: baseRegex) else { return nil }
         let range = match.range
-        return URL(string: path.replacingCharacters(in: range, with: ""))!
+        return URL(string: path.replacingCharacters(in: range, with: ""))
     }
 }
 
