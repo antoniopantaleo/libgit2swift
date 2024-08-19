2bcdac8a0c20e83a557a6eefbd7d57a4a2016694
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-08-19 13:14:27 +0200

    test(url-extensions): use XCTUnwrap to avoid force-unwrap exclamation mark

--- Tests/Libgit2SwiftTests/Internal/URLExtensionsTests.swift
+++ Tests/Libgit2SwiftTests/Internal/URLExtensionsTests.swift
@@ -10,27 +10,27 @@ import XCTest
 
 final class URLExtensionsTests: XCTestCase {
     
-    func test_pathRelative_toParentDirectory() {
-        let baseURL = URL(string: "/var/lib")!
-        let sut = URL(string: "/var/lib/myFile.txt")!
+    func test_pathRelative_toParentDirectory() throws {
+        let baseURL = try XCTUnwrap(URL(string: "/var/lib"))
+        let sut = try XCTUnwrap(URL(string: "/var/lib/myFile.txt"))
         XCTAssertEqual(sut.path(relativeTo: baseURL)?.path(percentEncoded: false), "myFile.txt")
     }
     
-    func test_pathRelative_toParentDirectory_withAdditionalDirectories() {
-        let baseURL = URL(string: "/var/lib")!
-        let sut = URL(string: "/var/lib/xcode/15.1/myFile.txt")!
+    func test_pathRelative_toParentDirectory_withAdditionalDirectories() throws {
+        let baseURL = try XCTUnwrap(URL(string: "/var/lib"))
+        let sut = try XCTUnwrap(URL(string: "/var/lib/xcode/15.1/myFile.txt"))
         XCTAssertEqual(sut.path(relativeTo: baseURL)?.path(percentEncoded: false), "xcode/15.1/myFile.txt")
     }
     
-    func test_pathRelative_toParentDirectory_withRepeatedOriginalDirectory() {
-        let baseURL = URL(string: "/var/lib")!
-        let sut = URL(string: "/var/lib/xcode/var/lib/myFile.txt")!
+    func test_pathRelative_toParentDirectory_withRepeatedOriginalDirectory() throws {
+        let baseURL = try XCTUnwrap(URL(string: "/var/lib"))
+        let sut = try XCTUnwrap(URL(string: "/var/lib/xcode/var/lib/myFile.txt"))
         XCTAssertEqual(sut.path(relativeTo: baseURL)?.path(percentEncoded: false), "xcode/var/lib/myFile.txt")
     }
     
-    func test_pathRelative_toParentDirectory_differentDirectories() {
-        let baseURL = URL(string: "/var/lib")!
-        let sut = URL(string: "/xcode/myFile.txt")!
+    func test_pathRelative_toParentDirectory_differentDirectories() throws {
+        let baseURL = try XCTUnwrap(URL(string: "/var/lib"))
+        let sut = try XCTUnwrap(URL(string: "/xcode/myFile.txt"))
         XCTAssertNil(sut.path(relativeTo: baseURL))
     }
 }
